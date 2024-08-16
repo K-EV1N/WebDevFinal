2ac3c511473b77a6aba9e6479f3918d3ae1dd943
Kevin Weng <kevinweng70@gmail.com>
2024-08-15 21:11:30 -0400

    implemented AddEmployee

--- src/pages/AddEmployee.jsx
+++ src/pages/AddEmployee.jsx
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addEmployee } from '../store/EmployeeRedux';
 import AddEmployeeCSS from '../styles/AddEmployee.module.css'
 
 function AddEmployee() {
+    const [firstname, setFirstName] = useState('');
+    const [lastname, setLastName] = useState('');
+    const [department, setDepartment] = useState('');
+    const dispatch = useDispatch();
+
+    const handleSubmit = () => {
+        if(firstname && lastname && department) {
+            const newEmployee = {
+                firstname,
+                lastname,
+                department
+            };
+
+            dispatch(addEmployee(newEmployee));
+            setFirstName('');
+            setLastName('');
+            setDepartment('');
+        } else {
+            alert('Please fill out all the fields.')
+        }
+    }
     return (
         <>
             <div className={AddEmployeeCSS['container']}>
-                <input className={AddEmployeeCSS['firstName']} placeholder='First Name'></input>
-                <input className={AddEmployeeCSS['lastName']} placeholder='Last Name'></input>
-                <input className={AddEmployeeCSS['department']} placeholder='Department'></input>
-                <button className={AddEmployeeCSS['addEmployee']} >Add Employee</button>
+                <h1 className={AddEmployeeCSS['header']}>Add Employee</h1>
+                <input 
+                    className={AddEmployeeCSS['firstName']} 
+                    placeholder='First Name'
+                    value={firstname}
+                    onChange={(e) => setFirstName(e.target.value)}
+                ></input>
+
+                <input 
+                    className={AddEmployeeCSS['lastName']} 
+                    placeholder='Last Name'
+                    value={lastname}
+                    onChange={(e) => setLastName(e.target.value)}
+                ></input>
+
+                <input 
+                    className={AddEmployeeCSS['department']} 
+                    placeholder='Department'
+                    value={department}
+                    onChange={(e) => setDepartment(e.target.value)}
+                ></input>
+                
+                <button className={AddEmployeeCSS['addEmployee']} onClick={handleSubmit} >Add Employee</button>
+                <Link to="/AllEmployeesView"><button className={AddEmployeeCSS['backButton']}>Back</button></Link>
             </div>
 
         </>
--- src/store/EmployeeRedux.js
+++ src/store/EmployeeRedux.js
@@ -4,6 +4,10 @@ export function EmployeeReducer(state = initialState, action) {
     switch(action.type) {
         case 'employees/employeesLoaded':
             return action.payload;
+        case 'employees/employeeDeleted':
+            return state.filter(employee => employee.id !== action.payload);
+        case 'employees/employeeAdded':
+            return[...state, action.payload]
         default:
             return state;
     }
@@ -21,4 +25,22 @@ export const fetchEmployees = () => async (dispatch) => {
         console.error(error);
     }
 };
+
+export const deleteEmployee = employeeId => async dispatch => {
+    try {
+        await axios.delete(`${PATH}/${employeeId}`);
+        dispatch({type: 'employees/employeeDeleted', payload: employeeId});
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+export const addEmployee = (employee) => async (dispatch) => {
+    try {
+        let response = await axios.post(`${PATH}`, employee);
+        dispatch({type: 'employees/employeeAdded', payload: response.data})
+    } catch(error) {
+        console.log(error)
+    }
+}
 
